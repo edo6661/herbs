2515c6f56c8e2700c282da87b88c11eddb67449b
Edo <muhammadridho5654@gmail.com>
2024-12-22 16:37:24 +0700

    feat: product items functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// nav
 const mobileNavContainer = document.querySelector(".container-mobile-nav");
 const body = document.querySelector("body");
 const toggleNavBtns = document.querySelectorAll(".toggle-nav-btn");
@@ -19,5 +20,45 @@ mobileNavContainer.addEventListener("click", (e) => {
 
   if (e.target === mobileNavContainer || e.target.closest("li")) {
     toggleMobileNav();
+  }
+});
+const items = document.querySelectorAll(".bottom-about-item");
+const modal = document.querySelector(".modal-container");
+const modalImage = modal.querySelector("img");
+const modalTitle = modal.querySelector("h2");
+const modalDescription = modal.querySelector("p");
+const closeModalBtn = modal.querySelector(".close-modal");
+
+function openModal(item) {
+  const imgSrc = item.querySelector("img").src;
+  const title = item.dataset.title;
+  const description = item.dataset.description;
+
+  modalImage.src = imgSrc;
+  modalTitle.textContent = title;
+  modalDescription.textContent = description;
+
+  modal.classList.remove("hidden");
+  modal.classList.add("active");
+  body.classList.add("overflow-hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  modal.classList.remove("active");
+  body.classList.remove("overflow-hidden");
+}
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    openModal(item);
+  });
+});
+
+closeModalBtn.addEventListener("click", closeModal);
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    closeModal();
   }
 });
