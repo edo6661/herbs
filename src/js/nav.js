318a91aa18dfd8336295fc2d44a611646dac7874
Edo <muhammadridho5654@gmail.com>
2024-12-23 13:01:07 +0700

    fix: nav anchor click

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,4 +1,28 @@
-export function toggleMobileNav() {
+function handleAnchorNav() {
+  const navLinks = document.querySelectorAll(
+    ".container-nav a[href^='#'], .container-mobile-nav a[href^='#']"
+  );
+  const offset = 92;
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log("test");
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        const targetPosition =
+          targetElement.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+}
+function toggleMobileNav() {
   const mobileNavContainer = document.querySelector(".container-mobile-nav");
   const heroActionsContainer = document.querySelector(".hero-action-container");
   const body = document.querySelector("body");
@@ -26,4 +50,5 @@ export function initNav() {
       toggleMobileNav();
     }
   });
+  handleAnchorNav();
 }
