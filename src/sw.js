ee7d91c8f06d76a3d346e268a4c38aca6ae6f19c
Edo <muhammadridho5654@gmail.com>
2025-01-10 02:05:58 +0700

    fix: sw

---
+++ src/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./styles/style.css",
+        "./js/main.js",
+        "./assets/images/logo-edited.webp",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
